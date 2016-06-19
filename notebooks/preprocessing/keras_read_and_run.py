80865f577f6b4a94a1db3f565acc19930b18ce70
hawkerpl <m.m.hawker@gmail.com>
2016-06-19 03:44:55 +0200

    fix keras run

--- notebooks/preprocessing/keras_read_and_run.py
+++ notebooks/preprocessing/keras_read_and_run.py
@@ -4,27 +4,34 @@ import os
 data_folder = '../data'
 images_folder = os.path.join(data_folder,'new_roof_images/resized')
 ids_path = '../data/new_roof_images/id_train_resampled.csv'
-
+#gpu run 
+#    THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32
 from dataset import Dataset
 dataset = Dataset(images_folder, ids_path=ids_path)
+#dataset.ids = dataset.ids[:100]
 dataset.read_dataset()
-
-print('X_train shape:', dataset.X_train.shape)
-print(dataset.X_train.shape[0], 'train samples')
-print(dataset.X_test.shape[0], 'test samples')
 
 def func(row):
     return row.T
 
 dataset.apply_each_X_data_row(func)
 
+
+print('X_train shape:', dataset.X_train.shape)
+print(dataset.X_train.shape[0], 'train samples')
+print(dataset.X_test.shape[0], 'test samples')
+
 batch_size = 128
 nb_epoch = 12
-
+from keras.models import model_from_json
 jmodel = ""
 with open('model.json') as f:
     jmodel = f.read()
 model = model_from_json(jmodel)
+
+model.compile(loss='categorical_crossentropy',
+              optimizer='adadelta',
+              metrics=['accuracy'])
 
 model.fit(dataset.X_train, dataset.Y_train, batch_size=batch_size, nb_epoch=nb_epoch,
           verbose=1, validation_data=(dataset.X_test, dataset.Y_test))
